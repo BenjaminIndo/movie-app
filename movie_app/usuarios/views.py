b9a6751912d5dc2d471812b76ea23fbb357a91f9
BenjaminIndo <benjamin.indo@ug.uchile.cl>
2025-04-29 20:14:38 -0400

    fix: bug in views.py

--- movie_app/usuarios/views.py
+++ movie_app/usuarios/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, redirect
 from usuarios.models import Pelicula
-# Create your views here.
+
 def peliculas(request):
     mis_peliculas = Pelicula.objects.all()
+
     if request.method == "GET":
-        return render(request, "usuarios/index.html", {"peliculas":mis_peliculas})
-    if request.method =="POST":
+        return render(request, "usuarios/index.html", {"peliculas": mis_peliculas})
+    
+    elif request.method == "POST":
         if "addMovie" in request.POST:
+            # Agregar nueva película
             titulo = request.POST["titulo"]
             anno = request.POST["anno"]
             genero = request.POST["genero"]
             duracion = request.POST["duracion"]
             director = request.POST["director"]
             descripcion = request.POST["descripcion"]
+
             nueva_pelicula = Pelicula(
                 titulo=titulo,
                 anno=anno,
@@ -21,15 +25,14 @@ def peliculas(request):
                 director=director,
                 descripcion=descripcion
             )
-            
-            # Guardar la nueva película en la base de datos
             nueva_pelicula.save()
-            
-            # Redirigir a la misma página para mostrar la lista actualizada de películas
-            return redirect('/')  # Asegúrate de que la vista 'peliculas' esté correctamente definida en urls.py
+
         elif "deleteMovie" in request.POST:
             # Eliminar películas seleccionadas
             ids_a_eliminar = request.POST.getlist("checkedbox")
             Pelicula.objects.filter(id__in=ids_a_eliminar).delete()
 
         return redirect('/')
+
+    # En caso de que no sea ni GET ni POST (por seguridad)
+    return redirect('/')
