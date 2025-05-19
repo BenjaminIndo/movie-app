9b47eb861f628533db28c0d2cc1268167d48d5f9
BenjaminIndo <benjamin.indo@ug.uchile.cl>
2025-05-18 22:57:06 -0400

    feat: add owner for peliculas

--- movie_app/usuarios/models.py
+++ movie_app/usuarios/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from account.models import User
 class Pelicula(models.Model):
     titulo = models.CharField(max_length=250)
     anno = models.IntegerField(default = 2024)
@@ -7,7 +7,7 @@ class Pelicula(models.Model):
     duracion = models.IntegerField(default = 0)
     director = models.CharField(max_length=250)
     descripcion = models.CharField(max_length=250)
-
+    owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
     def __str__(self):
         return self.titulo
 # Create your models here.
--- movie_app/usuarios/views.py
+++ movie_app/usuarios/views.py
@@ -16,7 +16,7 @@ def peliculas(request):
             duracion = request.POST["duracion"]
             director = request.POST["director"]
             descripcion = request.POST["descripcion"]
-
+            
             nueva_pelicula = Pelicula(
                 titulo=titulo,
                 anno=anno,
@@ -26,7 +26,26 @@ def peliculas(request):
                 descripcion=descripcion
             )
             nueva_pelicula.save()
-
+            if request.user.is_authenticated:
+                nueva_pelicula = Pelicula(
+                    titulo=titulo,
+                    anno=anno,
+                    genero=genero,
+                    duracion=duracion,
+                    director=director,
+                    descripcion=descripcion,
+                    owner=request.user
+                )
+            else:
+                nueva_pelicula = Pelicula(
+                    titulo=titulo,
+                    anno=anno,
+                    genero=genero,
+                    duracion=duracion,
+                    director=director,
+                    descripcion=descripcion
+                )
+            nueva_pelicula.save()
         elif "deleteMovie" in request.POST:
             # Eliminar películas seleccionadas
             ids_a_eliminar = request.POST.getlist("checkedbox")
